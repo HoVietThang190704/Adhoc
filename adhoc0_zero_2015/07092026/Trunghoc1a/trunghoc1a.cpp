67fe0df4e384fd2d2187d73c6a00035a44e30009
Hồ Việt Thắng <154068613+HoVietThang190704@users.noreply.github.com>
2026-09-07 22:57:46 +0700

    update trunghoc1a

--- adhoc0_zero_2015/07092026/Trunghoc1a/trunghoc1a.cpp
+++ adhoc0_zero_2015/07092026/Trunghoc1a/trunghoc1a.cpp
@@ -18,9 +18,6 @@ int main() {
   int **matrix = new int*[n];
   for (int i = 0; i < n; i++) {
     matrix[i] = new int[n];
-  }
-
-  for (int i = 0; i < n; i++) {
     for (int j = 0; j < n; j++) {
       inputFile >> matrix[i][j];
     }
